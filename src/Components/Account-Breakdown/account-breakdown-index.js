0027ab8b5f2224611fe33ef40504f436e8d5cdf3
ChrisJMurdoch <christopherjmurdoch@gmail.com>
2020-10-03 15:58:05 +0100

    Added pie chart

--- src/Components/Account-Breakdown/account-breakdown-index.js
+++ src/Components/Account-Breakdown/account-breakdown-index.js
@@ -1,4 +1,5 @@
 import React, {Component} from "react";
+import { PieChart } from 'react-minimal-pie-chart';
 /* Someone working for the account page can work from here*
 * Strong urge to use Material UI components
 * https://material-ui.com/components/tabs/#tabs - Example
@@ -7,9 +8,44 @@ import React, {Component} from "react";
 * */
 class AccountBreakdownIndex extends Component {
     render() {
+
+        const s1 = {
+            height:'500px'
+        }
+
         return (
             <div>
-                Account
+                <PieChart
+                    animation
+                    animationDuration={500}
+                    animationEasing="ease-out"
+                    center={[50, 50]}
+                    data={[
+                        {
+                        color: "#E38627",
+                        title: "One",
+                        value: 10,
+                        },
+                        {
+                        color: "#C13C37",
+                        title: "Two",
+                        value: 15,
+                        },
+                        {
+                        color: "#6A2135",
+                        title: "Three",
+                        value: 20,
+                        },
+                    ]}
+                    labelPosition={50}
+                    lengthAngle={360}
+                    lineWidth={15}
+                    paddingAngle={0}
+                    radius={50}
+                    rounded
+                    startAngle={0}
+                    viewBoxSize={[100, 100]}
+                />
             </div>
         )
     }
