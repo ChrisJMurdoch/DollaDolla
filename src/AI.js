982e0d9e512bf9ae4c02991b5bc69ba5f983468f
ChrisJMurdoch <christopherjmurdoch@gmail.com>
2020-10-04 01:24:21 +0100

    Improved AI responses

--- src/AI.js
+++ src/AI.js
@@ -1,4 +1,5 @@
 
+/** Eliza-style chatbot class */
 export default class AI {
 
     balance;
@@ -16,30 +17,44 @@ export default class AI {
         // Ignore case
         input = input.toLowerCase();
 
-        // Name query
-        if (input.includes("name"))
-            return "My name is HAL.";
+        // Loop through keyword=>intent mappings
+        for (let set of this.keywords ) {
 
-        // Balance query
-        if (input.includes("balance"))
-            return "Your balance is: £" + this.balance + ".";
+            // Loop through keywords in set
+            for (let keyword of set.aliases) {
 
-        // Goal query
-        else if(input.includes("goal"))
-            return "You are £" + (this.goal-this.balance) + " from your goal.";
+                // Call intent if matched
+                if (input.includes(keyword)) {
+                    return set.intent();
+                }
+            }
+        }
 
-        // Spending
-        else if(input.includes("netflix"))
-            return "You have spent £" + this.spending["netflix"] + " on Netflix this month.";
-
-        // ...
-        else if(input.includes("open"))
-            return "I'm sorry Dave, I'm afraid I can't do that.";
-            
-        // Spending query | Netflix
-        else
-            return "I'm sorry, I don't understand.";
-
+        // Call default intent if no matches
+        return this.intents.default();
     }
+
+    // Map intents to replies
+    intents = {
+        greet               : () => "Hello!",
+        leave               : () => "Goodbye!",
+        ask_name            : () => "My name is HAL.",
+        ask_balance         : () => "Your balance is: £" + this.balance + ".",
+        ask_goal            : () => "You are £" + (this.goal-this.balance) + " from your goal.",
+        ask_netflix_balance : () => "You have spent £" + this.spending["netflix"] + " on Netflix this month.",
+        ask_open            : () => "I'm sorry Dave, I'm afraid I can't do that.",
+        default             : () => "I'm sorry, I don't understand.",
+    };
+
+    // Map keywords to intents
+    keywords = [
+        { aliases: [ "hi", "hello" ],            intent: this.intents['greet'] },
+        { aliases: [ "bye", "goodbye" ],         intent: this.intents['leave'] },
+        { aliases: [ "name", "call" ],           intent: this.intents['ask_name'] },
+        { aliases: [ "balance" ],                intent: this.intents['ask_balance'] },
+        { aliases: [ "goal", "left", "target" ], intent: this.intents['ask_goal'] },
+        { aliases: [ "netflix" ],                intent: this.intents['ask_netflix_balance'] },
+        { aliases: [ "open", "pod bay doors" ],  intent: this.intents['ask_name'] },
+    ];
 
 };
